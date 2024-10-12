d1da661be12bd76fc8c4cf346f9bb4a3b8f547cc
raghveerdeol <deolraghveer5@gmail.com>
2024-10-12 22:09:25 +0200

    edited card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,12 +14,14 @@ export default {
     <div class="container-fluid">
         <div class="row">
             <div class="card col-3" style="width: 18rem;"  v-for="repository in store.repositories">
-                <img src="..." class="card-img-top" alt="...">
+                <img :src="repository.owner.avatar_url" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title"> {{ repository.name }} </h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                        content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h6 class="card-title"><em> {{ repository.full_name }} </em></h6>
+                    <span class="badge text-bg-success"> {{ repository.language }} </span>
+                    <p><em> {{ repository.created_at }} </em></p>
+                    <p>Visibility:<em> {{ repository.visibility }} </em></p>
+                    <a :href="repository.html_url" class="btn btn-primary" target="_blank" rel="noopener noreferrer" >Go to repository</a>
                 </div>
             </div>
         </div>
