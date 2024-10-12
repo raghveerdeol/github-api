5cc838c5b82cb16eb1b1bdd2bf884d9f9e360bbd
raghveerdeol <deolraghveer5@gmail.com>
2024-10-12 21:47:03 +0200

    search by repository name

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,53 @@
 <script>
+import { store }  from '../store';
+import axios from 'axios';
+
 export default {
     data() {
         return {
-
+            githubApi: 'https://api.github.com/search/repositories',
+            store,
+            valueInput: '',
         }
-    }
+    },
+    methods: {
+        getRepos(api, name) {
+            axios.get(api, {
+                params: {
+                    q: name,
+                    per_page: 30,
+                }
+            })
+                .then((response) => {
+                    this.store.repositories = response.data.items;
+                    console.log(this.store.repositories);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        }
+    },
+    // created() {
+    //     this.getRepos(this.githubApi, this.valueInput);
+    // }
 }
 </script>
 
 <template>
     <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+            <form class="d-flex" role="search" @submit.prevent="getRepos(this.githubApi, this.valueInput)" >
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="search" v-model="valueInput">
+                <button class="btn btn-outline-success">Search</button>
             </form>
         </div>
     </nav>
+
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,46 +1,27 @@
 <script>
 import axios from 'axios';
-
+import { store } from "../store";
 export default {
     data() {
         return {
-            githubApi: 'https://api.github.com/search/repositories'
+            store,
         }
     },
-    methods: {
-        getRepos() {
-            axios.get('https://api.github.com/search/repositories', {
-                params: {
-                    q: 'profume',
-                    per_page: 30,
-                }
-            })
-                .then(function (response) {
-                    console.log(response.data.items);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                })
-                .finally(function () {
-                    // always executed
-                });
-        }
-    },
-    created() {
-        this.getRepos();
-    }
-
 }
 </script>
 
 <template>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="card col-3" style="width: 18rem;"  v-for="repository in store.repositories">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> {{ repository.name }} </h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                        content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
