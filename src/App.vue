7ebeee2fc9b1222b5204a4ced13d67c5eabb9e06
raghveerdeol <deolraghveer5@gmail.com>
2024-10-12 16:27:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppHeader from './components/Header.vue';
+import AppMain from './components/Main.vue';
+import AppFooter from './components/Footer.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+
+  <main>
+    <AppMain />
+  </main>
+
+  <footer>
+    <AppFooter />
+  </footer>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios';
+
+export default {
+data() {
+    return {
+        githubApi: 'https://api.github.com/search/repositoriess'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>
+        
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
